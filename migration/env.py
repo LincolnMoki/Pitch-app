2a7999fca5cd64ece9ad1d889077b9ce0337bdeb
Ronny <mwithiamoki@gmail.com>
2020-09-21 02:31:26 +0300

    define run migration online

--- migration/env.py
+++ migration/env.py
@@ -35,3 +35,39 @@ def run_migrations_offline():
 
     with context.begin_transaction():
         context.run_migrations()
+
+
+
+
+def run_migrations_online():
+    
+    def process_revision_directives(context, revision, directives):
+        if getattr(config.cmd_opts, 'autogenerate', False):
+            script = directives[0]
+            if script.upgrade_ops.is_empty():
+                directives[:] = []
+                logger.info('No changes in schema detected.')
+
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            process_revision_directives=process_revision_directives,
+            **current_app.extensions['migrate'].configure_args
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
+Tom
