c043d2d45bf87617db5c381a7bcb61598235817c
Ronny <mwithiamoki@gmail.com>
2020-09-21 02:09:07 +0300

    updated class config

--- manage.py
+++ manage.py
@@ -17,7 +17,22 @@ def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2)
+@manager.shell
+def add_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'Comment': Comment}
+
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == "__main__":
+        manager.run()
+.run(tests)
 
 @manager.shell
 def make_shell_context():
