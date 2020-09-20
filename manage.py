49c9de2d3f96529294f49d3a7083a15145fec273
Ronny <mwithiamoki@gmail.com>
2020-09-21 02:06:25 +0300

    add created instance

--- manage.py
+++ manage.py
@@ -12,17 +12,16 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-@manager.shell
-def add_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Comment': Comment}
-
-
 @manager.command
 def test():
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
 
-if __name__ == "__main__":
-        manager.run()
+if __name__ == '__main__':
+    manager.run()
