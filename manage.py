4d971a5f661e83719ed3e41555d80e5fa1cd1aff
Ronny <mwithiamoki@gmail.com>
2020-09-21 02:05:51 +0300

    add created instance

--- manage.py
+++ manage.py
@@ -1,18 +1,16 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Server
 
-from app import create_app
-from app import db
-from app.models import User, Post, Comment
 
 app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server', Server)
+manager.add_command('server',Server)
 
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
-
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def add_shell_context():
