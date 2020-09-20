c6048ff0097435f91a97dbb78e49e6aa5d2af5a3
Ronny <mwithiamoki@gmail.com>
2020-09-21 02:38:54 +0300

    updates

--- main/forms.py
+++ main/forms.py
@@ -6,12 +6,12 @@ from wtforms.validators import Required
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[Required()])
     post = TextAreaField('Pitch', validators=[Required()])
-    category = SelectField('Category', choices=[('product', 'product'), ('idea', 'idea'), ('business', 'business')],
+    category = SelectField('Category', choices=[('product', 'product'), ('sport', 'sport'), ('business', 'business')],
                            validators=[Required()])
     submit = SubmitField('Post')
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('Comment', validators=[Required()])
+    comment = TextAreaField('leave a comment:', validators=[Required()])
     submit = SubmitField('Post')
 
 
