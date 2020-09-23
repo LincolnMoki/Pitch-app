a2a4fd70cf338957891a7dce4004fc5813e554e4
Ronny <mwithiamoki@gmail.com>
2020-09-23 12:34:15 +0300

    updates

--- manage.py
+++ manage.py
@@ -14,17 +14,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2)
-@manager.shell
-def add_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Comment': Comment}
-
-
-@manager.command
-def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
